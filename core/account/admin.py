67e7b8b2f1e7465084e9b35d33fcb4c4aa6bd47b
rajan2rajan <rajanbhandari939@gmail.com>
2023-06-20 02:17:19 +0545

    adding group

--- core/account/admin.py
+++ core/account/admin.py
@@ -1,3 +1,46 @@
 from django.contrib import admin
 
 # Register your models here.
+"""django admin costumization"""
+
+from account import models
+from django.utils.translation import gettext_lazy as _
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+
+class UserAdmin(BaseUserAdmin):
+    """this is for showing user page in admin """
+    ordering = ['id']
+    list_display = ['email', 'name']
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (
+            _('Permissions'),
+            {
+                'fields': (
+                    'is_active',
+                    'is_staff',
+                    'is_superuser',
+                )
+            }
+        ),
+        (_('Important dates what you want to show as message'), {'fields': ('last_login',)}),
+    )
+    readonly_fields = ['last_login']
+    """from this is to show the page of add user in admin page"""
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': (
+                'email',
+                'password1',
+                'password2',
+                'name',
+                'is_active',
+                'is_staff',
+                'is_superuser',
+            )
+        }),
+    )
+
+
+admin.site.register(models.User,UserAdmin)
--- core/account/tests.py
+++ core/account/tests.py
@@ -1,5 +1,7 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
+from django.urls import reverse
+from django.test import Client
 # Create your tests here.
 
 
@@ -9,7 +11,7 @@ from django.contrib.auth import get_user_model
 
 # create() is used to create a new User object with the username "jane", the specified password, and the is_staff flag set to True
 
-class Createusername(TestCase):
+class CreateUserName(TestCase):
     def test_create_with_email(self):
         email = "rajan@example.com"
         password = "rajan123"
@@ -34,3 +36,19 @@ class Createusername(TestCase):
         )
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
+
+
+class AdminSiteTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.admin_user = get_user_model().objects.create_superuser(
+            email = 'admin@example.com',
+            password = 'admin123'
+        )
+        self.client.force_login(self.admin_user)
+        self.user = get_user_model().objects.create_user(
+            email = 'user@example.com',
+            password = 'test123',
+            name = 'user1'
+        )
+
